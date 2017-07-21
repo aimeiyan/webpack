c9540e2dce1bb132b9a0379481dd0fa00dd38c29
aimeiyan <aimeiyan@gmail.com>
2017-07-22 07:28:19 +0800

    --

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,14 +2,40 @@ var htmlWebpackPlugin=require('html-webpack-plugin');
 
 module.exports={
 	entry:{
-		page1:__dirname+'/src/script/main.js',
-		page2:__dirname+'/src/script/a.js'	
+		main:__dirname+'/src/script/main.js',
+		a:__dirname+'/src/script/a.js',
+		b:__dirname+'/src/script/b.js',
+		c:__dirname+'/src/script/c.js'	
 	},
 	output:{
-		path:__dirname+'/dist/js',
-		filename:'[name]-[chunkhash].js'
+		path:__dirname+'/dist',
+		filename:'js/[name]-[chunkhash].js',
+		publicPath:'http://cdn.com/'
 	},
 	plugins:[
-		new htmlWebpackPlugin()
+		new htmlWebpackPlugin({
+			filename:'a.html',
+			template:'index.html',
+			inject:'body',
+			title:"this is a.html",
+			// chunks:['a','main'],
+			excludeChunks:['b','c']
+		}),
+		new htmlWebpackPlugin({
+			filename:'b.html',
+			template:'index.html',
+			inject:'body',
+			title:"this is b.html",
+			// chunks:['b'],
+			excludeChunks:['a','c']
+		}),
+		new htmlWebpackPlugin({
+			filename:'c.html',
+			template:'index.html',
+			inject:'body',
+			title:"this is c.html",
+			// chunks:['c']
+			excludeChunks:['a','b']
+		})
 	]
 }	
